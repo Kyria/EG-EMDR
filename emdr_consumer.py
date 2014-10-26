cbbf95590f7443aa3887510ea7d27f7804868377
Guillaume <anakhon@gmail.com>
2014-10-26 18:12:07 +0100

    Fix type error when values are null (weird)
    + close connection when required

--- emdr_consumer.py
+++ emdr_consumer.py
@@ -73,39 +73,42 @@ class EMDRConsumer:
         
         cursor = db.cursor()
 
-        for row in rowset['rows']:
-            order = self.init_data(columns, row, generated_at, type_id, region_id)
-        
-            order['issueDate'] = parser.parse(order['issueDate'])
-            if order['solarSystemID'] in settings.SOLAR_SYSTEMS:
-                if order['solarSystemID'] not in by_system:
-                    by_system[order['solarSystemID']] = []
-                by_system[order['solarSystemID']].append(order)
-            else:
-                continue
-
-        for system, orders in by_system.iteritems():
-            sort = sorted(orders, key=lambda x: x["bid"])
+        try:
+            for row in rowset['rows']:
+                order = self.init_data(columns, row, generated_at, type_id, region_id)
             
-            bids = {k:list(g) for k,g in groupby(sort, key=lambda x: x["bid"])}
-            max_bid = max(o['price'] for o in bids[True]) if True in bids else None
-            min_sell = min(o['price'] for o in bids[False]) if False in bids else None
-            vol_remaining = sum(o['volRemaining'] for o in bids[False]) if False in bids else None  
-            vol_entered = sum(o['volEntered'] for o in bids[False]) if False in bids else None  
-            orders_number = int(len(orders))
-            
-            try:
-                cursor.execute(self.order_query,(type_id, generated_at, orders_number, min_sell, max_bid, vol_remaining, vol_entered, region_id, system))
-                db.commit()
-            except MySQLdb.Error as err:
-                db.rollback()
-                error_code = err[0]
-                if error_code == self.DUPLICATE_ENTRY_ERROR_CODE:
-                    cursor.execute(self.order_query_update,(generated_at, orders_number, min_sell, max_bid, vol_remaining, vol_entered, system, type_id))
-                    db.commit()
+                order['issueDate'] = parser.parse(order['issueDate'])
+                if order['solarSystemID'] in settings.SOLAR_SYSTEMS:
+                    if order['solarSystemID'] not in by_system:
+                        by_system[order['solarSystemID']] = []
+                    by_system[order['solarSystemID']].append(order)
                 else:
-                    message = err[1]
-                    logger.error("MySQL Error : %s - %s" % (error_code, message))
+                    continue
+
+            for system, orders in by_system.iteritems():
+                sort = sorted(orders, key=lambda x: x["bid"])
+                
+                bids = {k:list(g) for k,g in groupby(sort, key=lambda x: x["bid"])}
+                max_bid = max(o['price'] for o in bids[True]) if True in bids else None
+                min_sell = min(o['price'] for o in bids[False]) if False in bids else None
+                vol_remaining = sum(o['volRemaining'] for o in bids[False]) if False in bids else None  
+                vol_entered = sum(o['volEntered'] for o in bids[False]) if False in bids else None  
+                orders_number = int(len(orders))
+                
+                try:
+                    cursor.execute(self.order_query,(type_id, generated_at, orders_number, min_sell, max_bid, vol_remaining, vol_entered, region_id, system))
+                    db.commit()
+                except MySQLdb.Error as err:
+                    db.rollback()
+                    error_code = err[0]
+                    if error_code == self.DUPLICATE_ENTRY_ERROR_CODE:
+                        cursor.execute(self.order_query_update,(generated_at, orders_number, min_sell, max_bid, vol_remaining, vol_entered, system, type_id))
+                        db.commit()
+                    else:
+                        message = err[1]
+                        logger.error("MySQL Error : %s - %s" % (error_code, message))
+        finally:
+            cursor.close()
 
     def insert_history(self, rowset, columns, generated_at, type_id, region_id, db):
         """
@@ -113,7 +116,7 @@ class EMDRConsumer:
         """
         if region_id not in settings.REGIONS:
             return
-        
+                
         query_values_hist = []
         for row in rowset['rows']:
             data = self.init_data(columns, row, generated_at, type_id, region_id)
@@ -123,8 +126,12 @@ class EMDRConsumer:
 
         if query_values_hist:
             cursor = db.cursor()
-            cursor.executemany(self.history_query,query_values_hist)
-            db.commit()
+            try:
+                cursor.executemany(self.history_query,query_values_hist)
+                db.commit()
+            finally:
+                cursor.close()
+        
    
     def init_data(self,columns, row, generated_at, type_id, region_id):
         """
@@ -156,19 +163,22 @@ class EMDRConsumer:
         db = MySQLdb.connect(settings.DB_HOST, settings.DB_USER, settings.DB_PASS, settings.DB_NAME)
         
         # let's start
-        for rowset in market_data['rowsets']:
+        try:
+            for rowset in market_data['rowsets']:
+                
+                generated_at = parser.parse(rowset['generatedAt']).strftime('%Y-%m-%d %H:%M:%S')
+                type_id = int(rowset['typeID'] or 0)
+                region_id = int(rowset['regionID'] or 0)
+                
+                # message is for history
+                if data_type == 'history' and settings.INSERT_HISTORY: 
+                    self.insert_history(rowset, columns, generated_at, type_id, region_id, db)
+                # the current message is for orders   
+                elif data_type == 'orders' and settings.INSERT_DAILY:
+                    self.insert_daily_value(rowset, columns, generated_at, type_id, region_id, db)
+        finally:
+            db.close()
             
-            generated_at = parser.parse(rowset['generatedAt']).strftime('%Y-%m-%d %H:%M:%S')
-            type_id = int(rowset['typeID'])
-            region_id = int(rowset['regionID'])
-            
-            # message is for history
-            if data_type == 'history' and settings.INSERT_HISTORY: 
-                self.insert_history(rowset, columns, generated_at, type_id, region_id, db)
-            # the current message is for orders   
-            elif data_type == 'orders' and settings.INSERT_DAILY:
-                self.insert_daily_value(rowset, columns, generated_at, type_id, region_id, db)
-    
     def purge_data(self):
         """
         Purge all useless data
@@ -188,11 +198,15 @@ class EMDRConsumer:
 
         logger.info("... purging price history older than %d days..." % (settings.HISTORY_DAYS_RETENTION,))
         # delete all history older than 365 day
-        cursor.execute("DELETE FROM emdr_price_history where `date` < (UTC_TIMESTAMP() - INTERVAL %d DAY)" % settings.HISTORY_DAYS_RETENTION)
-             
-        # commit :)
-        db.commit()
-        
+        try:
+            cursor.execute("DELETE FROM emdr_price_history where `date` < (UTC_TIMESTAMP() - INTERVAL %d DAY)" % settings.HISTORY_DAYS_RETENTION)
+                 
+            # commit :)
+            db.commit()
+        finally:
+            cursor.close()
+            db.close()
+            
         logger.info("... Purging done")
     
     
